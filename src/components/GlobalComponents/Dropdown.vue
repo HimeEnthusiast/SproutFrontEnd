65e9eebfa386b0d9baa6875aaf09dee9599b9068
Isabella Piantoni <piantois@sheridancollege.ca>
2020-06-07 22:48:51 -0400

    Changing api links

--- src/components/GlobalComponents/Dropdown.vue
+++ src/components/GlobalComponents/Dropdown.vue
@@ -78,7 +78,7 @@
             }
         },
         mounted() {
-            axios.post(`${process.env.DOMAIN_NAME}/api/subcategory-per-category`, {
+            axios.post(`${process.env.DOMAIN_NAME}/subcategory-per-category`, {
                 category: this.category
             }).then((response) => {
                 response.data.forEach(x => {
