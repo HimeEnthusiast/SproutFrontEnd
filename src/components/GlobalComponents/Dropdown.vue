ed50372635f7f92b24c5cf33420a86bce7c4b445
Isabella Piantoni <piantois@sheridancollege.ca>
2020-06-10 14:31:43 -0400

    Changing some POST requests to GET

--- src/components/GlobalComponents/Dropdown.vue
+++ src/components/GlobalComponents/Dropdown.vue
@@ -78,24 +78,6 @@
             }
         },
         mounted() {
-            const getSubs = async() => {
-                try {
-                    const res = await axios.get("https://www.isabellapiantoni.tech/api/subcategory-per-category/" + this.category, { 
-                        headers: {
-                            'Content-Type' : 'application/json'
-                        }
-                    })
-
-                    // const subs = res.data();
-                    alert(res.data);
-                }
-                catch (e) {
-                    console.error(e);
-                }
-            }
-
-            getSubs();
-
             // axios.get("https://www.isabellapiantoni.tech/api/subcategory-per-category/" + this.category, {
             //     headers: {
             //         'Content-Type' : 'application/json'
@@ -110,17 +92,17 @@
             //     console.log(error);
             // });
 
-            // const apiClient = axios.create({ 
-            //     baseURL: 'https://isabellapiantoni.tech/api', 
-            //     headers: {
-            //         'Accept': 'application/json', 
-            //         'Content-Type': 'application/json'
-            //     }});
+            const apiClient = axios.create({ 
+                baseURL: 'https://isabellapiantoni.tech', 
+                headers: {
+                    'Accept': 'application/json', 
+                    'Content-Type': 'application/json'
+                }});
 
-            //     apiClient.get("/subcategory-per-category/" + this.category)
-            //         .then((response) => {
-            //             alert(response.data);
-            //         });
+                apiClient.get("/api/subcategory-per-category/" + this.category)
+                    .then((response) => {
+                        alert(response.data);
+                    });
         },
         methods: {
             drop: function() {
