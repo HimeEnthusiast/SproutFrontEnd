95cffa690519050c2578d58bd3758046b4557a07
Isabella Piantoni <piantois@sheridancollege.ca>
2020-06-11 14:25:17 -0400

    Fixing request errors

--- src/components/GlobalComponents/Dropdown.vue
+++ src/components/GlobalComponents/Dropdown.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="root">
         <div id="title" ref="title" @mouseover="drop" @mouseleave="close"> 
-            <router-link class="router-link" :to="{name: 'products-category', params: {category: category.replace(/\s+/g, '-').toLowerCase(), subcategory: null}}">{{title}}</router-link>
+            <router-link class="router-link" :to="{name: 'products-category', params: {category: category.replace(/\s+/g, '-').toLowerCase(), subcategory: ''}}">{{title}}</router-link>
         </div>
 
         <div id="dropdown" ref="dropdown" @mouseover="drop" @mouseleave="close">
